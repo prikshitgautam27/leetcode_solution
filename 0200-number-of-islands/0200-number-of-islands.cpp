3db0bd2728f9f694fae4418b024293171518991d
Prikshit Gautam <135122621+prikshitgautam27@users.noreply.github.com>
2026-04-30 11:51:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int n=grid[0].size();int count=0;
-        int m=grid.size();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]=='1'){
-                    dfs(grid,i,j);
+        int count =0;
+        int n= grid.size();
+        int m= grid[0].size();
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j] == '1'){
+                    dfs(grid, i, j);
                     count++;
                 }
+
             }
         }
-return count ;
+return count;
     }
-    void dfs(vector<vector<char>>&grid,int i,int j){
-            int n=grid[0].size(); 
-        int m=grid.size();
-if(i<0||i>=m||j<0||j>=n||grid[i][j]!='1') return ;
-grid[i][j]='0';
-dfs(grid,i+1,j);
-dfs(grid,i-1,j);
-dfs(grid,i,j-1);
-dfs(grid,i,j+1);
 
+    void dfs(vector<vector<char>>& grid, int i, int j){
+        int n= grid.size();
+        int m= grid[0].size();
+
+        if(i<0 || i>=grid.size() || j<0 || j>=m || grid[i][j] == '0') return ;
+
+        grid[i][j]='0';// marking it as visited
+
+int dr[4]={-1,0, 1, 0};
+int dc[4] = {0, 1 , 0 ,-1};
+        for(int k=0;k<4;k++){
+            dfs(grid, i+dr[k], j + dc[k]);
+        }
 
     }
 };
