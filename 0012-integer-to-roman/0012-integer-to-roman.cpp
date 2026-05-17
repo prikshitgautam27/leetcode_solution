c65083d873bf668acd69ef074a7e6576a4fb2eb9
Prikshit Gautam <135122621+prikshitgautam27@users.noreply.github.com>
2026-05-17 18:24:56 +0530

    Time: 4 ms (50.29%), Space: 12.8 MB (15.7%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,26 @@
+
+
+class Solution {
+public:
+      string intToRoman(int num) {
+        // Pairs of values and their corresponding Roman symbols 
+        // in descending order, including the special subtractive cases.
+     vector<std::pair<int, std::string>> romanMap = {
+            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+            {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
+            {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
+        };
+
+       string result = "";
+
+        for (const auto& [value, symbol] : romanMap) {
+            // While the current value can be subtracted from num
+            while (num >= value) {
+                result += symbol;
+                num -= value;
+            }
+        }
+
+        return result;
+    }
+};
