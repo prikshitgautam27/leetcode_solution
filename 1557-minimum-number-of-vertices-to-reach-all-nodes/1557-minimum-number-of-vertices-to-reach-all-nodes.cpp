bd2bf704f8f10ce445fcdfaec77baffa48403630
Prikshit Gautam <135122621+prikshitgautam27@users.noreply.github.com>
2026-07-24 17:53:37 +0530

    Time: 49 ms (34.85%), Space: 111.6 MB (20.29%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
-      
+    // calculating the indegree
         vector<int>indegree(n,0);
         for(auto it:edges){
             int u=it[0];
@@ -9,6 +9,7 @@ public:
             
             indegree[v]++;
         }
+        // calculating the result from nodes having the indegree 0
         vector<int>res;
         for(int i=0;i<n;i++){
             if(indegree[i]==0){
